d9ae37bf5aacae133f608b016a250ce6eeccf9c2
tiidawork0727 <tiidawork0727@gmail.com>
2023-05-13 18:05:26 +0900

    version up

--- DisSaruBot_js/mugen_recruitment.js
+++ DisSaruBot_js/mugen_recruitment.js
@@ -1,43 +1,52 @@
-// const Discord = require('discord.js');
-// const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
-
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers] });
 
+// ボットのトークン
+const token = '';
+
+// 送信するチャンネルのID
+const channelId = '644877926290161707';
+
+// 毎日送信する時間（24時間表記の時刻）
+const sendTime = '17:30:00';
+
+// ボットがログインした時に実行される処理
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
+
+    // 指定した時刻にメッセージを送信するための処理
+    const now = new Date();
+    const timeToSend = new Date(
+        now.getFullYear(),
+        now.getMonth(),
+        now.getDate(),
+        sendTime.split(':')[0],
+        sendTime.split(':')[1],
+        sendTime.split(':')[2]
+    );
+
+    const timeUntilSend = timeToSend.getTime() - now.getTime();
+    if (timeUntilSend < 0) {
+        timeToSend.setDate(timeToSend.getDate() + 1);
+    }
+
+    setTimeout(() => {
+        sendDailyMessage();
+        setInterval(sendDailyMessage, 24 * 60 * 60 * 1000);
+    }, timeUntilSend);
 });
 
-// メッセージを送信する関数
-function sendMessage() {
-  const guild = client.guilds.cache.get('644877926290161704'); // サーバーIDを指定
-  const channel = guild.channels.cache.get('644877926290161707'); // チャンネルIDを指定
-  channel.send('ムゲン放置狩り 21:00~22:00 (1枠目)'); // メッセージを送信
-  channel.send('ムゲン放置狩り 21:00~22:00 (2枠目)'); // メッセージを送信
+// メッセージを送信する処理
+function sendDailyMessage() {
+    const now = new Date();
+    const dateString = now.getFullYear() + ('00' + (now.getMonth() + 1)).slice(-2) + ('00' + now.getDate()).slice(-2);
+
+    // メッセージの送信
+    client.channels.cache.get(channelId).send(dateString)
+        .then(() => console.log(`Sent message for ${dateString}`))
+        .catch((error) => console.error(`Error sending message: ${error}`));
 }
 
-// 毎日19時にメッセージを送信する関数
-function scheduleDailyMessage(hour, minute) {
-  const now = new Date();
-  const target = new Date();
-  // const target = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 19, 0, 0);
-  target.setHours(hour, minute, 0)
-
-  if (target.getTime() <= now.getTime()) {
-    // 指定した時間がすでに過ぎている場合、翌日の同じ時間に送信する
-    target.setDate(target.getDate() + 1);
-  }
-
-  const delay = target.getTime() - now.getTime();
-  setTimeout(() => {
-    sendMessage();
-    scheduleDailyMessage(); // 次回送信をスケジュールする
-  }, delay);
-}
-
-// ログインする
-client.login('');
-
-// メッセージを送信時刻設定
-scheduleDailyMessage(17, 20);
+// ボットのログイン
+client.login(token);
