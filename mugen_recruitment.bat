9d20983552a9d2314bd235757ec6d0e0dae3e4ea
tiidawork0727 <tiidawork0727@gmail.com>
2023-05-18 00:50:50 +0900

    create bat files

---
+++ mugen_recruitment.bat
@@ -0,0 +1,17 @@
+@echo off
+cls
+
+set /a scheduled_time=1648
+
+:loop
+set current_time=%time:~0,2%%time:~3,2%
+if %current_time% lss 100 (
+    set current_time=24%current_time%
+)
+
+if %current_time% equ %scheduled_time% (
+    node C:\work\DisSaruBot_js\mugen_recruitment.js
+)
+
+ping -n 60 127.0.0.1 > nul
+goto loop
